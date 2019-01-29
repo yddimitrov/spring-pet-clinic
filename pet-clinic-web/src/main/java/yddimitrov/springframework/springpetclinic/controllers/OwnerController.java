37b49ddb046d399909d720c700b41044871126e7
Yordan Dimitrov <tengambg@gmail.com>
2019-01-29 13:59:56 +0200

    Added owners template and controller

---
+++ pet-clinic-web/src/main/java/yddimitrov/springframework/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package yddimitrov.springframework.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listVets() {
+        return "owners/index.html";
+    }
+}
