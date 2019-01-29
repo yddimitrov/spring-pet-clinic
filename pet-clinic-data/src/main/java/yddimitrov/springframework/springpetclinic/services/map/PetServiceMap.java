cfcb7ca7d91b913741ec63c57746216b50673672
Yordan Dimitrov <tengambg@gmail.com>
2019-01-29 13:45:05 +0200

    Added Map services

---
+++ pet-clinic-data/src/main/java/yddimitrov/springframework/springpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,32 @@
+package yddimitrov.springframework.springpetclinic.services.map;
+
+import yddimitrov.springframework.springpetclinic.model.Pet;
+import yddimitrov.springframework.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) { super.deleteById(id); }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/yddimitrov/springframework/springpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package yddimitrov.springframework.springpetclinic.services.map;
+
+import yddimitrov.springframework.springpetclinic.model.Owner;
+import yddimitrov.springframework.springpetclinic.model.Vet;
+import yddimitrov.springframework.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) { super.deleteById(id); }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
